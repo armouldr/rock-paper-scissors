a6ea5c33c04c6e29f1c7287f5babc6dedc1e5aab
armould <arnaud.rajon@epfl.ch>
2023-06-29 23:38:04 +0200

    first version of the UI

--- script.js
+++ script.js
@@ -1,69 +1,90 @@
 const dict = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    let index = Math.floor(Math.random() * 3);
-    return dict[index];
+  let index = Math.floor(Math.random() * 3);
+  return dict[index];
 }
 
 function playRound(playerSelection, computerSelection) {
+  console.assert(playerSelection !== null);
 
-    playerSelection = playerSelection.toLowerCase();
-    playerSelection = playerSelection.replace(playerSelection.charAt(0), 
-                      playerSelection.charAt(0).toUpperCase());
+  playerSelection = playerSelection.toLowerCase();
+  playerSelection = playerSelection.replace(
+    playerSelection.charAt(0),
+    playerSelection.charAt(0).toUpperCase()
+  );
 
-    let playerSelectionInd = dict.indexOf(playerSelection);
+  let playerSelectionInd = dict.indexOf(playerSelection);
+
+  let computerSelectionInd = dict.indexOf(computerSelection);
+
+  if (playerSelectionInd === -1 || computerSelectionInd === -1) {
     console.log(playerSelection);
-    console.log(computerSelection)
+    console.log(computerSelection);
+    console.error("There is an error");
+  }
 
-    let computerSelectionInd = dict.indexOf(computerSelection);
-
-    if (playerSelectionInd === -1 || computerSelectionInd === -1) {
-        console.log(playerSelection);
-        console.log(computerSelection);
-        console.error("There is an error");
-    }
-
-    if (playerSelectionInd === computerSelectionInd) {
-        return 0;
-
-    } else if (computerSelectionInd === (playerSelectionInd + 1)%3) {
-        return -1;
-
-    } else {
-        return 1;
-    }
-
+  if (playerSelectionInd === computerSelectionInd) {
+    return 0;
+  } else if (computerSelectionInd === (playerSelectionInd + 1) % 3) {
+    return -1;
+  } else {
+    return 1;
+  }
 }
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let playerSelection, computerSelection;
+  let playerScore = 0;
+  let computerScore = 0;
+  let playerSelection, computerSelection;
+  let result;
 
-    for(let i=0; i < 5; ++i) {
-
-        playerSelection = prompt("Rock, paper or scissors ?");
+  const buttons = Array.from(document.getElementsByClassName("but"));
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      if (run) {
         computerSelection = getComputerChoice();
+        playerSelection = event.target.id;
+        result = playRound(playerSelection, computerSelection);
+        [playerScore, computerScore] = updateScore(
+          playerScore,
+          computerScore,
+          result
+        );
+        displayScore(playerScore, computerScore);
+      }
+    });
+  });
+}
 
-        let result = playRound(playerSelection, computerSelection);
-        if (result === 1) {
-            ++playerScore;
-            console.log("You Won this round !");
-        } else if (result === -1) {
-            ++computerScore;
-            console.log("You lose this round !");
-        } else {
-            console.log("This round's a tie !");
-        }
+function updateScore(playerScore, computerScore, result) {
+  const board = document.getElementById("result");
+  if (result === 1) {
+    ++playerScore;
+    board.textContent = "You won this round !";
+  } else if (result === -1) {
+    ++computerScore;
+
+    board.textContent = "You lose this round !";
+  } else {
+    board.textContent = "This round's a tie !";
+  }
+  if (playerScore >= 5 || computerScore >= 5) {
+    run = false;
+
+    if (playerScore > computerScore) {
+      board.textContent = "The player wins !";
+    } else {
+      board.textContent = "Computer takes the win ! beep bip bop";
     }
-    displayScore(playerScore, computerScore);
-
+  }
+  return [playerScore, computerScore];
 }
 
 function displayScore(playerScore, computerScore) {
-    console.log("Final Score :");
-    console.log(`Player : ${playerScore}`);
-    console.log(`Player : ${computerScore}`);
+  const score = document.getElementById("score");
+  score.textContent = "onche";
+  score.textContent = `Player : ${playerScore}           Computer : ${computerScore}`;
 }
-
+let run = true;
 game();
